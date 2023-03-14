c892d2289d3d0a41fbb31c8b76282dfc5bdb0475
Pedro Soares <pmbs.123@gmail.com>
2023-03-14 21:45:41 +0100

    [feat] Create a config file for ros parameters

--- src/unitree_ros_node.cpp
+++ src/unitree_ros_node.cpp
@@ -1,10 +1,10 @@
 #include <rclcpp/rclcpp.hpp>
-#include "unitree_ros/unitree_ros.hpp"
+#include "unitree_ros/unitree_driver_ros.hpp"
 #include "unitree_legged_sdk/unitree_legged_sdk.h"
 
 int main(int argc, char* argv[]) {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<UnitreeRos>());
+    // rclcpp::spin(std::make_shared<UnitreeRos>());
     rclcpp::shutdown();
     return 0;
 }

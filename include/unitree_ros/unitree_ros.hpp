9693a47f1197496b3f4a834e9881237285337524
Pedro Soares <pmbs.123@gmail.com>
2023-03-14 21:46:10 +0100

    [misc] Renamed file to unitree_driver_ros.hpp

--- include/unitree_ros/unitree_ros.hpp
+++
@@ -1,8 +0,0 @@
-#include <rclcpp/rclcpp.hpp>
-
-class UnitreeRos: public rclcpp::Node{
-    public:
-        UnitreeRos() : Node("unitree_ros_node"){
-            RCLCPP_INFO(this->get_logger(), "Node is alive");
-        }
-};

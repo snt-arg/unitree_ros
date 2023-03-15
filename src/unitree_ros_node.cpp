dd839961a6e4c701e8b683d3d519999941aa0270
Pedro Soares <pmbs.123@gmail.com>
2023-03-15 22:19:17 +0100

    [refactor] Update to the new name of the driver class

--- src/unitree_ros_node.cpp
+++ src/unitree_ros_node.cpp
@@ -1,10 +1,11 @@
 #include <rclcpp/rclcpp.hpp>
-#include "unitree_ros/unitree_driver_ros.hpp"
+
 #include "unitree_legged_sdk/unitree_legged_sdk.h"
+#include "unitree_ros/unitree_driver_ros.hpp"
 
 int main(int argc, char* argv[]) {
     rclcpp::init(argc, argv);
-    // rclcpp::spin(std::make_shared<UnitreeRos>());
+    rclcpp::spin(std::make_shared<UnitreeDriverRos>());
     rclcpp::shutdown();
     return 0;
 }

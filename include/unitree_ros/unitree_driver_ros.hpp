69729bb9d182a9c8e17869120b6138af72f94911
Pedro Soares <pmbs.123@gmail.com>
2023-03-16 14:09:37 +0100

    [refactor] Initialized class attributes

--- include/unitree_ros/unitree_driver_ros.hpp
+++ include/unitree_ros/unitree_driver_ros.hpp
@@ -12,19 +12,20 @@
 
 class UnitreeDriverRos : public rclcpp::Node {
    private:
-    std::string cmd_vel_topic_name;
-    std::string odometry_topic_name;
-    std::string imu_topic_name;
-    std::string robot_ip;
-    int robot_local_port, robot_target_port;
+    std::string cmd_vel_topic_name = "/cmd_vel";
+    std::string odometry_topic_name = "/odom";
+    std::string imu_topic_name = "/imu";
+    std::string robot_ip = "192.168.12.1";
+    int robot_local_port = 8090;
+    int robot_target_port = 8082;
 
-   private:
-    // This connection is used for High level commands only
+   public:
+    // UDP connection used for High level commands
     UNITREE_LEGGED_SDK::UDP robotUDPConnection;
     // Unitree High Command struct
-    UNITREE_LEGGED_SDK::HighCmd robotHighCmd;
+    UNITREE_LEGGED_SDK::HighCmd robotHighCmd = {};
     // Unitree High State struct
-    UNITREE_LEGGED_SDK::HighState robotHighState;
+    UNITREE_LEGGED_SDK::HighState robotHighState = {};
 
    private:
     rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odomPub;
@@ -45,8 +46,14 @@ class UnitreeDriverRos : public rclcpp::Node {
     void robotStateTimerCallback();
     /**
      * @brief Callback method which receives velocity commands and controls the robot
+     *
+     * @params msg: Twist message
      */
     void cmdVelCallback(const geometry_msgs::msg::Twist::SharedPtr msg);
+
+    /**
+     * @brief Method which reads ros_params and updates the variables values.
+     */
     void readParams();
 };
 

5ff06c09d4da3b146f7e6abdbf66532112ac5d1b
Pedro Soares <pmbs.123@gmail.com>
2023-03-17 19:30:44 +0100

    [refactor] Cleaned the code

--- include/unitree_ros/unitree_driver_ros.hpp
+++ include/unitree_ros/unitree_driver_ros.hpp
@@ -1,36 +1,50 @@
 #ifndef UNITREE_DRIVER_ROS_HPP
 #define UNITREE_DRIVER_ROS_HPP
 
-#include <geometry_msgs/msg/twist.hpp>
-#include <nav_msgs/msg/odometry.hpp>
 #include <rclcpp/rclcpp.hpp>
-#include <sensor_msgs/msg/imu.hpp>
-#include <std_msgs/msg/int32.hpp>
-#include <unitree_ros/msg/high_cmd.hpp>
 
 #include "unitree_legged_sdk/unitree_legged_sdk.h"
+#include "unitree_ros/conversion.hpp"
+
+// Ros Messages
+#include <geometry_msgs/msg/twist.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <sensor_msgs/msg/imu.hpp>
+#include <unitree_ros/msg/bms_state.hpp>
 
 class UnitreeDriverRos : public rclcpp::Node {
+    // Parameters
    private:
-    std::string cmd_vel_topic_name = "/cmd_vel";
-    std::string odometry_topic_name = "/odom";
-    std::string imu_topic_name = "/imu";
-    std::string robot_ip = "192.168.12.1";
-    int robot_local_port = 8090;
-    int robot_target_port = 8082;
+    std::string cmdVelTopicName = "/cmd_vel";
+    std::string odometryTopicName = "/odom";
+    std::string odometryFrameId = "Odometry";
+    std::string imuTopicName = "/imu";
+    std::string imuFrameId = "IMU";
+    std::string robotIP = "192.168.12.1";
+    int robotLocalPort = 8090;
+    int robotTargetPort = 8082;
 
+    // Unitree Related
    public:
     // UDP connection used for High level commands
     UNITREE_LEGGED_SDK::UDP robotUDPConnection;
     // Unitree High Command struct
     UNITREE_LEGGED_SDK::HighCmd robotHighCmd = {};
     // Unitree High State struct
-    UNITREE_LEGGED_SDK::HighState *robotHighState;
+    UNITREE_LEGGED_SDK::HighState robotHighState = {};
 
+    // Publishers
    private:
     rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odomPub;
     rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imuPub;
+    rclcpp::Publisher<unitree_ros::msg::BmsState>::SharedPtr batteryStatePub;
+
+    // Subscriptions
+   private:
     rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmdVelSub;
+
+    // Timers
+   private:
     rclcpp::TimerBase::SharedPtr robotStateTimer;
 
    public:
@@ -44,6 +58,7 @@ class UnitreeDriverRos : public rclcpp::Node {
      * @brief Callback function which publishes the odometry and imu data
      */
     void robotStateTimerCallback();
+
     /**
      * @brief Callback method which receives velocity commands and controls the robot
      *

c09fc6b6ca33ac1ae79027f690a421e18ed028d0
Pedro Soares <pmbs.123@gmail.com>
2023-03-17 19:28:29 +0100

    [feat] Create function to generate both the imu and odometry msg

--- include/unitree_ros/conversion.hpp
+++ include/unitree_ros/conversion.hpp
@@ -3,65 +3,26 @@
 
 #include <unitree_legged_sdk/unitree_legged_sdk.h>
 
+#include <nav_msgs/msg/detail/odometry__struct.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+// Ros Messages
 #include <geometry_msgs/msg/twist.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <sensor_msgs/msg/detail/imu__struct.hpp>
+#include <sensor_msgs/msg/imu.hpp>
 #include <unitree_ros/msg/bms_cmd.hpp>
 #include <unitree_ros/msg/high_cmd.hpp>
 
-inline UNITREE_LEGGED_SDK::BmsCmd rosMsg2Cmd(const unitree_ros::msg::BmsCmd &msg) {
-    UNITREE_LEGGED_SDK::BmsCmd cmd;
+#include "unitree_legged_sdk/comm.h"
 
-    cmd.off = msg.off;
-
-    for (int i(0); i < 3; i++) {
-        cmd.reserve[i] = msg.reserve[i];
-    }
-
-    return cmd;
-}
-
-inline UNITREE_LEGGED_SDK::HighCmd rosMsg2Cmd(
-    const unitree_ros::msg::HighCmd::SharedPtr &msg) {
-    UNITREE_LEGGED_SDK::HighCmd cmd;
-
-    for (int i(0); i < 2; i++) {
-        cmd.head[i] = msg->head[i];
-        cmd.SN[i] = msg->sn[i];
-        cmd.version[i] = msg->version[i];
-        cmd.position[i] = msg->position[i];
-        cmd.velocity[i] = msg->velocity[i];
-    }
-
-    for (int i(0); i < 3; i++) {
-        cmd.euler[i] = msg->euler[i];
-    }
-
-    for (int i(0); i < 4; i++) {
-        cmd.led[i].r = msg->led[i].r;
-        cmd.led[i].g = msg->led[i].g;
-        cmd.led[i].b = msg->led[i].b;
-    }
-
-    for (int i(0); i < 40; i++) {
-        cmd.wirelessRemote[i] = msg->wireless_remote[i];
-    }
-
-    cmd.levelFlag = msg->level_flag;
-    cmd.frameReserve = msg->frame_reserve;
-    cmd.bandWidth = msg->bandwidth;
-    cmd.mode = msg->mode;
-    cmd.gaitType = msg->gait_type;
-    cmd.speedLevel = msg->speed_level;
-    cmd.footRaiseHeight = msg->foot_raise_height;
-    cmd.bodyHeight = msg->body_height;
-    cmd.yawSpeed = msg->yaw_speed;
-    cmd.reserve = msg->reserve;
-    cmd.crc = msg->crc;
-
-    cmd.bms = rosMsg2Cmd(msg->bms);
-
-    return cmd;
-}
-
+/**
+ * @brief Update the high level command from a Twist message to send to robot
+ *
+ * @param msg: Twist message
+ *
+ * @return updated High level command
+ */
 inline UNITREE_LEGGED_SDK::HighCmd rosMsg2Cmd(
     const geometry_msgs::msg::Twist::SharedPtr msg) {
     UNITREE_LEGGED_SDK::HighCmd cmd;
@@ -69,7 +30,6 @@ inline UNITREE_LEGGED_SDK::HighCmd rosMsg2Cmd(
     cmd.head[0] = 0xFE;
     cmd.head[1] = 0xEF;
     cmd.levelFlag = UNITREE_LEGGED_SDK::HIGHLEVEL;
-    cmd.mode = 0;
     cmd.gaitType = 0;
     cmd.speedLevel = 0;
     cmd.footRaiseHeight = 0;
@@ -90,6 +50,67 @@ inline UNITREE_LEGGED_SDK::HighCmd rosMsg2Cmd(
     cmd.gaitType = 1;
 
     return cmd;
+}
+
+/**
+ * @brief Create an IMU message from the robot high state
+ *
+ * @param highState: high state struct received from robot
+ * @param now: time stamp of the message
+ * @param frame_id: frame id of the message
+ *
+ * @return imu message
+ */
+inline sensor_msgs::msg::Imu generateImuMsg(UNITREE_LEGGED_SDK::HighState &highState,
+                                            rclcpp::Time now,
+                                            std::string frame_id) {
+    sensor_msgs::msg::Imu imuStateMsg;
+
+    imuStateMsg.header.frame_id = frame_id;
+    imuStateMsg.header.stamp = now;
+    imuStateMsg.orientation.x = highState.imu.quaternion[0];
+    imuStateMsg.orientation.y = highState.imu.quaternion[1];
+    imuStateMsg.orientation.z = highState.imu.quaternion[2];
+    imuStateMsg.orientation.w = highState.imu.quaternion[3];
+    imuStateMsg.linear_acceleration.x = highState.imu.accelerometer[0];
+    imuStateMsg.linear_acceleration.y = highState.imu.accelerometer[1];
+    imuStateMsg.linear_acceleration.z = highState.imu.accelerometer[2];
+    imuStateMsg.angular_velocity.x = highState.imu.gyroscope[0];
+    imuStateMsg.angular_velocity.y = highState.imu.gyroscope[1];
+    imuStateMsg.angular_velocity.z = highState.imu.gyroscope[2];
+    return imuStateMsg;
+}
+
+/**
+ * @brief Create an odometry message from the robot high state
+ *
+ * @param highState: high state struct received from robot
+ * @param now: time stamp of the message
+ * @param frame_id: frame id of the message
+ *
+ * @return odometry message
+ */
+inline nav_msgs::msg::Odometry generateOdometryMsg(
+    UNITREE_LEGGED_SDK::HighState &highState,
+    rclcpp::Time now,
+    std::string frame_id) {
+    nav_msgs::msg::Odometry odometryStateMsg;
+
+    odometryStateMsg.header.frame_id = frame_id;
+    odometryStateMsg.header.stamp = now;
+    odometryStateMsg.twist.twist.linear.x = highState.velocity[0];
+    odometryStateMsg.twist.twist.linear.y = highState.velocity[1];
+    odometryStateMsg.twist.twist.linear.z = highState.velocity[2];
+    odometryStateMsg.twist.twist.angular.z = highState.yawSpeed;
+    odometryStateMsg.pose.pose.position.x = highState.position[0];
+    odometryStateMsg.pose.pose.position.y = highState.position[1];
+    odometryStateMsg.pose.pose.position.z = highState.position[2];
+    odometryStateMsg.pose.pose.orientation.x = highState.imu.quaternion[0];
+    odometryStateMsg.pose.pose.orientation.y = highState.imu.quaternion[1];
+    odometryStateMsg.pose.pose.orientation.z = highState.imu.quaternion[2];
+    odometryStateMsg.pose.pose.orientation.w = highState.imu.quaternion[3];
+
+    return odometryStateMsg;
 }
 
 #endif  // !CONVERSION_HPP

d46587e56fd0f14e622a81302edead12b5d754e9
Pedro Soares <pmbs.123@gmail.com>
2023-03-29 19:07:05 +0200

    [feat] Add the params file as a launch argument

--- launch/unitree_driver.launch.py
+++ launch/unitree_driver.launch.py
@@ -4,20 +4,23 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
     pkg_dir = get_package_share_directory("unitree_ros")
-    param_file = os.path.join(pkg_dir, "config", "params.yaml")
+    default_param_file = os.path.join(pkg_dir, "config", "params.yaml")
+    params_file = LaunchConfiguration("params_file")
+    params_file_arg = DeclareLaunchArgument(
+        "params_file", default_value=str(default_param_file)
+    )
 
     unitree_driver_node = Node(
         package="unitree_ros",
         executable="unitree_driver",
-        parameters=[param_file],
+        parameters=[params_file],
         output="screen",
     )
 
-    ld = LaunchDescription()
-    ld.add_action(unitree_driver_node)
-
-    return ld
+    return LaunchDescription([params_file_arg, unitree_driver_node])

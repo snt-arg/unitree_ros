fb6b4a80691de215f37810685d711212b89e0d64
Pedro Soares <pmbs.123@gmail.com>
2023-04-24 19:00:08 +0200

    [reafactor] Improve the launch file

--- launch/unitree_driver.launch.py
+++ launch/unitree_driver.launch.py
@@ -1,20 +1,31 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
+import yaml
 
-from launch import LaunchDescription
+from launch import LaunchDescription, LaunchContext
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
+from launch.actions import OpaqueFunction
 
 
 def generate_launch_description():
     pkg_dir = get_package_share_directory("unitree_ros")
     default_param_file = os.path.join(pkg_dir, "config", "params.yaml")
-    params_file = LaunchConfiguration("params_file")
     params_file_arg = DeclareLaunchArgument(
         "params_file", default_value=str(default_param_file)
     )
+    unitree_driver_function = OpaqueFunction(function=launch_unitree_driver)
+
+    return LaunchDescription([params_file_arg, unitree_driver_function])
+
+
+def launch_unitree_driver(context, *args, **kwargs):
+    params_file = LaunchConfiguration("params_file")
+
+    with open(params_file.perform(context)) as f:
+        params = yaml.safe_load(f)["unitree_driver_node"]["ros__parameters"]
 
     unitree_driver_node = Node(
         package="unitree_ros",
@@ -26,10 +37,18 @@ def generate_launch_description():
     static_transform_node = Node(
         package="tf2_ros",
         executable="static_transform_publisher",
-        arguments=["0", "0", "0", "0", "0", "0", "1", "base_link", "os_lidar"],
+        arguments=[
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "1",
+            params["odometry_child_frame_id"],
+            "os_lidar",
+        ],
         output="screen",
     )
 
-    return LaunchDescription(
-        [params_file_arg, unitree_driver_node, static_transform_node]
-    )
+    return [unitree_driver_node, static_transform_node]

a0f5641378ce70a038c5baae1656c579c199329d
Pedro Soares <pmbs.123@gmail.com>
2023-05-09 18:32:55 +0200

    [feat] Implement a simple obstacle avoidance logic.
    For this simple implementaion, when a velocity command is received, the ranges of
    the front, left and right sensors are checked. If we are moving forward and an object is
    in front, the velocity command will then be ignored. Same principle is applied to the other directions.

--- src/unitree_driver_ros.cpp
+++ src/unitree_driver_ros.cpp
@@ -10,6 +10,7 @@
 
 #include "unitree_legged_sdk/comm.h"
 #include "unitree_ros/conversion.hpp"
+#include "unitree_ros/utils.hpp"
 
 using namespace std::chrono_literals;
 
@@ -21,28 +22,15 @@ UnitreeDriverRos::UnitreeDriverRos()
                          false,
                          UNITREE_LEGGED_SDK::RecvEnum::nonBlock,
                          true) {
-    readParams();
-
     robotUDPConnection.SetIpPort(robotIP.c_str(), robotTargetPort);
     robotUDPConnection.InitCmdData(robotHighCmd);
 
+    readParams();
+    initPublishers();
+    initSubscriptions();
+    initTimers();
+
     tf_broadcaster = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
-
-    cmdVelSub = create_subscription<geometry_msgs::msg::Twist>(
-        cmdVelTopicName,
-        10,
-        std::bind(&UnitreeDriverRos::cmdVelCallback, this, std::placeholders::_1));
-
-    imuPub = create_publisher<sensor_msgs::msg::Imu>(imuTopicName, 10);
-    odomPub = create_publisher<nav_msgs::msg::Odometry>(odometryTopicName, 10);
-    batteryStatePub =
-        create_publisher<unitree_ros::msg::BmsState>(bmsStateTopicName, 10);
-
-    robotStateTimer = create_wall_timer(
-        2ms, std::bind(&UnitreeDriverRos::robotStateTimerCallback, this));
-
-    cmdVelResetTimer = create_wall_timer(
-        1ms, std::bind(&UnitreeDriverRos::cmdVelResetTimerCallback, this));
 
     RCLCPP_INFO(get_logger(), "Unitree Driver Node is ready!");
 }
@@ -51,17 +39,17 @@ void UnitreeDriverRos::readParams() {
     RCLCPP_INFO(get_logger(), "Reading ROS parameters...");
 
     // Robot params
-    declare_parameter<std::string>("robot_ip", "192.168.12.1");
-    declare_parameter<int>("robot_target_port", 8082);
+    declare_parameter<std::string>("robot_ip", robotIP);
+    declare_parameter<int>("robot_target_port", robotTargetPort);
     // --------------------------------------------------------
     get_parameter("robot_ip", robotIP);
     get_parameter("robot_target_port", robotTargetPort);
 
     // Topic Names
-    declare_parameter<std::string>("cmd_vel_topic_name", "cmd_vel");
-    declare_parameter<std::string>("imu_topic_name", "/imu");
-    declare_parameter<std::string>("odometry_topic_name", "/odom");
-    declare_parameter<std::string>("bms_state_topic_name", "/bms_state");
+    declare_parameter<std::string>("cmd_vel_topic_name", cmdVelTopicName);
+    declare_parameter<std::string>("imu_topic_name", imuTopicName);
+    declare_parameter<std::string>("odometry_topic_name", odometryTopicName);
+    declare_parameter<std::string>("bms_state_topic_name", bmsStateTopicName);
     // --------------------------------------------------------
     get_parameter("cmd_vel_topic_name", cmdVelTopicName);
     get_parameter("odometry_topic_name", odometryTopicName);
@@ -69,15 +57,40 @@ void UnitreeDriverRos::readParams() {
     get_parameter("bms_state_topic_name", bmsStateTopicName);
 
     // Frame Ids
-    declare_parameter<std::string>("imu_frame_id", "IMU");
-    declare_parameter<std::string>("odometry_frame_id", "odom");
-    declare_parameter<std::string>("odometry_child_frame_id", "base_link");
+    declare_parameter<std::string>("imu_frame_id", imuFrameId);
+    declare_parameter<std::string>("odometry_frame_id", odometryFrameId);
+    declare_parameter<std::string>("odometry_child_frame_id", odometryChildFrameId);
     // --------------------------------------------------------
     get_parameter("odometry_frame_id", odometryFrameId);
     get_parameter("odometry_child_frame_id", odometryChildFrameId);
     get_parameter("imu_frame_id", imuFrameId);
 
+    // Flags
+    declare_parameter<bool>("enable_obstacle_avoidance", enableObstacleAvoidance);
+
     RCLCPP_INFO(get_logger(), "Finished reading ROS parameters!");
+}
+
+void UnitreeDriverRos::initPublishers() {
+    imuPub = create_publisher<sensor_msgs::msg::Imu>(imuTopicName, 10);
+    odomPub = create_publisher<nav_msgs::msg::Odometry>(odometryTopicName, 10);
+    batteryStatePub =
+        create_publisher<unitree_ros::msg::BmsState>(bmsStateTopicName, 10);
+}
+
+void UnitreeDriverRos::initSubscriptions() {
+    cmdVelSub = create_subscription<geometry_msgs::msg::Twist>(
+        cmdVelTopicName,
+        10,
+        std::bind(&UnitreeDriverRos::cmdVelCallback, this, std::placeholders::_1));
+}
+
+void UnitreeDriverRos::initTimers() {
+    robotStateTimer = create_wall_timer(
+        2ms, std::bind(&UnitreeDriverRos::robotStateTimerCallback, this));
+
+    cmdVelResetTimer = create_wall_timer(
+        1ms, std::bind(&UnitreeDriverRos::cmdVelResetTimerCallback, this));
 }
 
 void UnitreeDriverRos::robotStateTimerCallback() {
@@ -88,10 +101,10 @@ void UnitreeDriverRos::robotStateTimerCallback() {
 
     rclcpp::Time now = this->get_clock()->now();
 
-    auto imuStateMsg = generateImuMsg(robotHighState, now, imuFrameId);
-    auto odometryStateMsg =
-        generateOdometryMsg(robotHighState, now, odometryFrameId, odometryChildFrameId);
-    auto batteryStateMsg = generateBatteryStateMsg(robotHighState);
+    auto imuStateMsg = convertIMUDataToMsg(robotHighState, now, imuFrameId);
+    auto odometryStateMsg = convertOdometryDataToMsg(
+        robotHighState, now, odometryFrameId, odometryChildFrameId);
+    auto batteryStateMsg = convertBMSDataToMsg(robotHighState);
     auto odomTransform = generateOdomTransform(
         odometryStateMsg, now, odometryFrameId, odometryChildFrameId);
 
@@ -102,7 +115,24 @@ void UnitreeDriverRos::robotStateTimerCallback() {
 }
 
 void UnitreeDriverRos::cmdVelCallback(const geometry_msgs::msg::Twist::SharedPtr msg) {
-    robotHighCmd = rosMsg2Cmd(msg);
+    robotHighCmd = convertTwistMsgToHighLevelCmd(msg);
+
+    // Check if the robot is in obstacle avoidance mode
+    if (enableObstacleAvoidance) {
+        float leftRange = robotHighState.rangeObstacle[0];
+        float rightRange = robotHighState.rangeObstacle[1];
+        float frontRange = robotHighState.rangeObstacle[2];
+
+        if (isCollisionPossible(frontRange, leftRange, rightRange)) {
+            if (msg->linear.x > 0 and frontRange < 0.4) {
+                robotHighCmd.velocity[0] = 0;
+            } else if (msg->linear.y > 0 and leftRange < 0.4) {
+                robotHighCmd.velocity[1] = 0;
+            } else if (msg->linear.y < 0 and rightRange < 0.4) {
+                robotHighCmd.velocity[1] = 0;
+            }
+        }
+    }
     robotUDPConnection.SetSend(robotHighCmd);
     robotUDPConnection.Send();
 
